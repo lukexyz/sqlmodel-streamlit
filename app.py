49ba86772ab143b78cbd7bbdfe61d3afa1b15d1e
lukexyz <lukedwoods@gmail.com>
2021-09-04 14:32:40 +0100

    create database and session

--- app.py
+++ app.py
@@ -1,6 +1,59 @@
 from typing import Optional
 
-from sqlmodel import Field, SQLModel
+import streamlit as st
+from sqlmodel import Field, Session, SQLModel, create_engine
+
 
 class Hero(SQLModel, table=True):
-    id
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: Optional[int] = None
+
+sqlite_file_name = 'database.db'
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+
+def create_heroes():
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+
+    print("Before interacting with the database")
+    print("Hero 1:", hero_1)
+    print("Hero 2:", hero_2)
+    print("Hero 3:", hero_3)
+
+    with Session(engine) as session:    
+        session.add(hero_1)
+        session.add(hero_2)
+        session.add(hero_3)
+
+        print("After adding to the session")
+        print("Hero 1:", hero_1)
+        print("Hero 2:", hero_2)
+        print("Hero 3:", hero_3)
+
+        session.commit()
+
+        print("After committing the session, show names")
+        print("Hero 1 name:", hero_1.name)
+        print("Hero 2 name:", hero_2.name)
+        print("Hero 3 name:", hero_3.name)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def main():
+    pass
+    #st.text('SQLModel')
+
+
+if __name__ == '__main__':
+    create_db_and_tables()
+    create_heroes()
+    main()
